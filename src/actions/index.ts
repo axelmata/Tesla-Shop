83b1c0a6b3c668abbcac0f4b68ba0f4be572f563
Axel Mata Ramirez <axel.1995@live.com.mx>
2023-12-10 11:24:54 -0600

    productos

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,6 @@
 
 
 
-export * from './product/product-pagination'
+export * from './product/get-product-by-slug';
+export * from './product/get-stock-by-slug';
+export * from './product/product-pagination';
